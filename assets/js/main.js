7f82ccad1a49a2227b695e9d85c9e517774c9ce5
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-09 01:32:53 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+let heroUndEnemy = [
+    ['Batman', 'the Joker'],
+    ['Professor X', 'Magento'],
+    ['Thor', 'Loki'],
+    ['Superman', 'Lex Luthor'],
+    ['Wolverine', 'Sabretooth'],
+    ['Ghost Rider', 'Mephisto']
+]
+
+console.log(heroUndEnemy)
+
+ 
